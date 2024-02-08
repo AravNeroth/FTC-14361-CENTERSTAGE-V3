9999d2597275295f95c267e28655d5acd228501a
AravNeroth <reacharav@outlook.com>
2024-02-08 11:43:24 -0600

    3 tests of new auto code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Autonomous/autoExecutable/AutoTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Autonomous/autoExecutable/AutoTest.java
@@ -1,6 +1,8 @@
 package org.firstinspires.ftc.teamcode.OpModes.Autonomous.autoExecutable;
 
+import com.acmerobotics.roadrunner.Action;
 import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
 import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
@@ -20,18 +22,33 @@ public class AutoTest {
             MecanumDrive drive = new MecanumDrive(hardwareMap, new Pose2d(new Vector2d(0.0, 0.0), Math.toRadians(90)));
 
             waitForStart();
+//         ----------------------------------------------
+//         still wondering wtf action is but hopefully this works !
+            Action okipullup = drive.actionBuilder(startPose)
+                    .splineToLinearHeading(new Pose2d(42, -36, 0), 0)
+                    .build();
 
-            Actions.runBlocking(new SequentialAction(
-                    MecanumDrive.FollowTrajectoryAction(Math.PI / 2),
-                    new ParallelAction(
-                            drive.followTrajectory(shootingTraj),
-                            new SequentialAction(
-                                    shooter.spinUp(),
-                                    shooter.fireBall()
-                            )
-                    )
-            ));
 
+            Actions.runBlocking(okipullup);
+//         ----------------------------------------------
+            /*
+            close left blue test! i put in some random ahh values
+            does this even make sense chat what teh hell is this new uodate
+             */
+//            Actions.runBlocking(new SequentialAction(
+//                            // Red Right Purple Right
+//                            drive.actionBuilder(new Pose2d(13, -60, Math.toRadians(90)))
+//                                    .splineTo(new Vector2d(13, -46), Math.toRadians(90))
+//                                    .splineTo(new Vector2d(18, -38), Math.toRadians(60))
+//                                    .build()
+//                    )
+//            );
+//          ---------------------------------------------
+//             Traj Action Builder test
+//            TrajectoryActionBuilder actionBuilder = drive.actionBuilder(drive.pose)
+//                    .strafeTo(new Vector2d(-42, -63))
+//                    .turn(0.00001)
+//                    .lineToY(-35);
 
         }
     }
