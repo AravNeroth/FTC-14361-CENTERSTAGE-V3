8b8d24310b297259c5e60e1efd0f28f3600aa1ab
AravNeroth <reacharav@outlook.com>
2024-02-09 10:04:28 -0600

    red detect VERY MUCH WORKING

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Autonomous/drive/TwoWheelTrackingLocalizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Autonomous/drive/TwoWheelTrackingLocalizer.java
@@ -61,7 +61,7 @@ public class TwoWheelTrackingLocalizer extends TwoTrackingWheelLocalizer {
         this.drive = drive;
 
         parallelEncoder = new Encoder(hardwareMap.get(DcMotorEx.class, "parallelEncoder"));
-        perpendicularEncoder = new Encoder(hardwareMap.get(DcMotorEx.class, "perpendicularEncoder"));
+        perpendicularEncoder = new Encoder(hardwareMap.get(DcMotorEx.class, "leftRear"));
         perpendicularEncoder.setDirection(Encoder.Direction.REVERSE);
 
         // TODO: reverse any encoders using Encoder.setDirection(Encoder.Direction.REVERSE)
